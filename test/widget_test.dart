7a3c2f2ca2b3138edf779e041ed6e0209a800984
Sanyam Ratreja <sanyamratreja18@gmail.com>
2024-01-14 12:02:46 +0530

    added web folder

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:airdroid/main.dart';
+import 'package:teencare/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
